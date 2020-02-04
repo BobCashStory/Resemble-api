56750fa66c98b8a83aaed7324f3c0d671c7d16c5
BobCashStory <47117399+BobCashStory@users.noreply.github.com>
2020-02-04 17:51:24 +0100

    Create getDiff.js

---
+++ controller/getDiff.js
@@ -0,0 +1,40 @@
+const puppeteer = require('resemblejs');
+const compareImages = require("resemblejs/compareImages");
+const fs = require("mz/fs");
+
+const defaultOptions = {
+        output: {
+            errorColor: {
+                red: 255,
+                green: 0,
+                blue: 255
+            },
+            errorType: "movement",
+            transparency: 0.3,
+            largeImageThreshold: 1200,
+            useCrossOrigin: false,
+            outputDiff: true
+        },
+        scaleToSameSize: true,
+        ignore: "antialiasing"
+    };
+    
+async function getDiff(image_1, image_2, options = defaultOptions) {
+    // The parameters can be Node Buffers
+    // data is the same as usual with an additional getBuffer() function
+    const data = await compareImages(
+        image_1,
+        image_2,
+        options
+    );
+    return {
+    diff: data.getBuffer(), 
+    score: {
+        misMatchPercentage: data.misMatchPercentage, 
+        isSameDimensions: data.isSameDimensions,
+        dimensionDifference: data.dimensionDifference
+      }
+    };
+}
+
+module.exports = getDiff;
